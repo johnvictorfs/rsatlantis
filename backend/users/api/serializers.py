5a5fa6390c22ce72bfbcc87c3c5523b0f8a039ad
John Victor <johnvictorfs@gmail.com>
2019-01-13 23:08:54 -0300

    Backend: Added endpoint /api/users/current/ to see details of the current logged in user.

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -7,4 +7,4 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'guides', 'username', 'groups')
+        fields = ('url', 'guides', 'username', 'email', 'groups', 'is_staff', 'is_superuser')
--- backend/users/api/views.py
+++ backend/users/api/views.py
@@ -16,7 +16,14 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['get'], permission_classes=[permissions.AllowAny])
     def guides(self, request, pk=None):
-        """Gets the url to the endpoint with the list of guides created by an User"""
+        """Endpoint with the list of guides created by an User"""
         queryset = Guide.objects.filter(author__pk=pk).order_by('-date_posted')
         serializer = GuideSerializer(queryset, context={'request': request}, many=True)
         return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def current(self, request):
+        """Details of the current logged in user"""
+        serializer = UserSerializer(request.user, context={'request': request})
+        return Response(serializer.data)
+
