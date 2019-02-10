28b26aacd64adc76bd81bb7e45128aa01c305f44
John Victor <johnvictorfs@gmail.com>
2019-02-10 13:18:21 -0300

    Backend: Removed STATICFILES_DIR from default settings.py.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,10 +134,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
