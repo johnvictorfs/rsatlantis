f7eb8d5551b7e814607d74218c311bf23ef511f6
John Victor <johnvictorfs@gmail.com>
2019-02-14 19:52:14 -0300

    Backend: Added translated_rank field to Clan Member's API.

--- backend/runescape/api/serializers.py
+++ backend/runescape/api/serializers.py
@@ -4,8 +4,28 @@ from runescape.models import ClanMember
 
 
 class ClanMemberSerializer(serializers.HyperlinkedModelSerializer):
+    translated_rank = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_translated_rank(obj):
+        ranks = {
+            "Owner": "Líder",
+            "Deputy Owner": "Vice-Líder",
+            "Overseer": "Fiscal",
+            "Coordinator": "Coordenador",
+            "Organiser": "Organizador",
+            "Admin": "Admin",
+            "General": "General",
+            "Captain": "Capitão",
+            "Lieutenant": "Tenente",
+            "Sergeant": "Sargento",
+            "Corporal": "Cabo",
+            "Recruit": "Recruta"
+        }
+        return ranks[obj.rank]
+
     class Meta:
         model = ClanMember
-        fields = ('url', 'name', 'exp', 'rank')
+        fields = ('url', 'name', 'exp', 'rank', 'translated_rank')
         read_only_fields = ('url', 'name', 'exp', 'rank')
         extra_kwargs = {'url': {'lookup_field': 'name'}}
