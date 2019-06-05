effa03f8f16636a9fe50d75b86024095d4fed208
John Victor <johnvictorfs@gmail.com>
2019-06-04 22:42:14 -0300

    Fixed build script not detecting errors in commands correctly

--- build.sh
+++ build.sh
@@ -8,14 +8,32 @@ NC=$'\e[0m'
 # Front-end Build #
 cd frontend/
 
-yarn install || echo "${RED}Could not install npm dependencies${NC}" && exit 1
-yarn build || echo "${RED}Could not build Vue Bundle${NC}" && exit 1
-cd ..
+if ! yarn install ; then
+  echo "${RED}Could not install npm dependencies${NC}"
+  exit 1
+fi
+
+if ! yarn build ; then
+  echo "${RED}Could not build Vue Bundle${NC}"
+  exit 1
+fi
 
 # Back-end Build
-cd backend/
+cd ../backend/
 
-poetry install || echo "${RED}Could not install pyproject.toml dependencies${NC}" && exit 1
-poetry run python manage.py migrate || echo "${RED}Could not migrate DB changes correctly${NC}" && exit 1
-poetry run python manage.py test || echo "${RED}Failure when running tests${NC}" && exit 1
-poetry run python manage.py collectstatic --noinput || echo "${RED}Could not run collectstatic management command${NC}" && exit 1
+if ! poetry install ; then
+  echo "${RED}Could not install pyproject.toml dependencies${NC}"
+  exit 1
+fi
+if ! poetry run python manage.py migrate ; then
+  echo "${RED}Could not migrate DB changes correctly${NC}"
+  exit 1
+fi
+if ! poetry run python manage.py test ; then
+  echo "${RED}Failure when running tests${NC}"
+  exit 1
+fi
+if ! poetry run python manage.py collectstatic --noinput ; then
+  echo "${RED}Could not run collectstatic management command${NC}"
+  exit 1
+fi
