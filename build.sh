a9788c9cc6849a4b00e9fe179521e3fcdba1a54c
John Victor <johnvictorfs@gmail.com>
2019-06-04 21:40:27 -0300

    Fixed issues with build script

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+ORANGE=$'\e[33m'
+GREEN=$'\e[32m'
+RED=$'\e[31m'
+NC=$'\e[0m'
+
 # Front-end Build #
 cd frontend/
 
-yarn install || exit 1 "Could not install npm dependencies"
-build || exit 1 "Could not build Vue Bundle"
+yarn install || echo "${RED}Could not install npm dependencies${NC}" && exit 1
+yarn build || echo "${RED}Could not build Vue Bundle${NC}" && exit 1
 cd ..
 
 # Back-end Build
 cd backend/
 
-poetry install || exit 1 "Could not install pyproject.toml dependencies"
-poetry run python manage.py migrate || exit 1 "Could not migrate DB changes correctly"
-poetry run python manage.py test || exit 1 "Failure when running tests"
-poetry run python manage.py collectstatic --noinput || exit 1 "Could not run collectstatic management command"
+poetry install || echo "${RED}Could not install pyproject.toml dependencies${NC}" && exit 1
+poetry run python manage.py migrate || echo "${RED}Could not migrate DB changes correctly${NC}" && exit 1
+poetry run python manage.py test || echo "${RED}Failure when running tests${NC}" && exit 1
+poetry run python manage.py collectstatic --noinput || echo "${RED}Could not run collectstatic management command${NC}" && exit 1
