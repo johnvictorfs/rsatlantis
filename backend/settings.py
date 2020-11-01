2605615300a86d6ec24b5c029582d4ec9e8883dc
John Victor <johnvfs@protonmail.com>
2020-11-01 19:55:53 -0300

    Separate 'discord' app into separate library in PyPi 'atlantisbot-api'

--- backend/settings.py
+++ backend/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'guides',
     'users',
     'runescape',
-    'discord',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
@@ -51,7 +50,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'drf_yasg',
     'dynamic_preferences',
-    'dynamic_preferences.users.apps.UserPreferencesConfig'
+    'dynamic_preferences.users.apps.UserPreferencesConfig',
+    'atlantisbot_api'
 ]
 
 MIDDLEWARE = [
